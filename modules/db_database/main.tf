abdda261684cd2176693d6a53ba1694a21b14fb6
lexwong <lexwong@lexwongdeMacBook-Air.local>
2019-09-24 17:12:45 +0800

    imporve(rds): update the module to the format of the new version.

--- modules/db_database/main.tf
+++ modules/db_database/main.tf
@@ -1,7 +1,6 @@
 resource "alicloud_db_database" "this_db_database" {
-
-  instance_id                  = "${var.instance_id}"
-  name                         = "${var.db_name}"
-  character_set                = "${var.character_set}"
-
+  instance_id   = var.instance_id
+  name          = var.db_name
+  character_set = var.character_set
 }
+
